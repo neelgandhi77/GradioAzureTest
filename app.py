3f9a136591f8fa8c579cdbd407f8514521224eab
Neelkumar Gandhi <63502709+neelgandhi77@users.noreply.github.com>
2024-08-22 09:42:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,18 @@
-# import gradio as gr
-#import os
-
-# def main():
-
-#     with gr.Blocks() as demo:
-#         gr.Markdown("Gradio -> working fine!")
-
-#     demo.launch()
-
-# """main Method Call"""
-# if __name__ == '__main__':
-#     main()
-
 from fastapi import FastAPI
 import gradio as gr
+import os
 
 app = FastAPI()
 
+def global_variable_value_change_show(value):
+    os.environ["MY_SECRET_KEY"] = value
+    return gr.Textbox(label="Global Variable Value Changed to: " + value,visible=True)
+
 with gr.Blocks() as demo:
-    gr.Markdown("Hey Innovation Team : Gradio -> working fine!")
+    gr.Markdown("Hey Team : Gradio -> working fine!")
+    global_variable_input = gr.Textbox(label="Input Global Variable")
+    current_value = gr.TextBox(os.environ.get("MY_SECRET_KEY"),visible=False)
+    submit_button = gr.Button("Submit")
+    submit_button.click(global_variable_value_change_show, inputs=[global_variable_input],outputs=[current_value])
 
 app = gr.mount_gradio_app(app, demo, path="/")
