8abf98159a72f3aa9f77abdf833720e969c107d1
Neelkumar Gandhi <63502709+neelgandhi77@users.noreply.github.com>
2024-08-22 14:38:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 import gradio as gr
 import os
 import json
+import config
+
+
+temp_list = []
 
 def string_to_list_format_generator(string):
     return string.split(', ')
@@ -12,7 +16,7 @@ def list_to_string_format_generator(list):
 # Load the JSON file
 with open('rules_modular_approach.json') as file:
     rules = json.load(file)
-temp_list = []
+
 app = FastAPI()
 
 def global_variable_value_change_show(value):
@@ -27,16 +31,19 @@ def global_variable_value_change_show(value):
     os.environ["MY_SECRET_KEY"] = temp_list
     #temp_list = string_to_list_format_generator(temp_list)
     #print(temp_list)
-
+    config.method = "Set by Neel : "+ str(value)
     return gr.Textbox(label="List : ", value= temp_list,visible=True), \
-            gr.Textbox(label="JSON : ", value= rules["data_preprocessing"]["duplicate value handling method selected by user"],visible=True)
+            gr.Textbox(label="JSON : ", value= rules["data_preprocessing"]["negative value handling method selected by user"],visible=True), \
+            gr.Textbox(label="Method : ", value=config.method ,visible=True)
 
 with gr.Blocks() as demo:
     gr.Markdown("Hey Team : Gradio -> working fine!")
     global_variable_input = gr.Textbox(label="Input Global Variable")
     current_value_list = gr.Textbox(os.environ.get("MY_SECRET_KEY"),visible=False)
     current_value_json = gr.Textbox(rules["data_preprocessing"]["negative value handling method selected by user"],visible=False)
+    current_value_method = gr.Textbox(config.method,visible=False)
+    
     submit_button = gr.Button("Submit")
-    submit_button.click(global_variable_value_change_show, inputs=[global_variable_input],outputs=[current_value_list,current_value_json])
+    submit_button.click(global_variable_value_change_show, inputs=[global_variable_input],outputs=[current_value_list,current_value_json,current_value_method])
 
 app = gr.mount_gradio_app(app, demo, path="/")
