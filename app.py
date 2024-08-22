f49d24416c159a8f7ea773c18f3365ab8fd85d8f
Neelkumar Gandhi <63502709+neelgandhi77@users.noreply.github.com>
2024-08-22 09:52:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def global_variable_value_change_show(value):
 with gr.Blocks() as demo:
     gr.Markdown("Hey Team : Gradio -> working fine!")
     global_variable_input = gr.Textbox(label="Input Global Variable")
-    current_value = gr.TextBox(os.environ.get("MY_SECRET_KEY"),visible=False)
+    current_value = gr.Textbox(os.environ.get("MY_SECRET_KEY"),visible=False)
     submit_button = gr.Button("Submit")
     submit_button.click(global_variable_value_change_show, inputs=[global_variable_input],outputs=[current_value])
 
