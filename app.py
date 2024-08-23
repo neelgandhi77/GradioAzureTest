7b003d30ebbaee16c64709a344940cee9dbaec3b
Neelkumar Gandhi <63502709+neelgandhi77@users.noreply.github.com>
2024-08-23 19:33:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 import json
 import config
 import pandas as pd
-from pycaret.regression import RegressionExperiment
+
 
 temp_list = []
 
@@ -14,16 +14,7 @@ def string_to_list_format_generator(string):
 def list_to_string_format_generator(list):
     return ', '.join(list)
     
-def pycaret_process(file_path):
-    dataset = pd.read_csv(file_path)
-    target_column = "SalePrice"
-    r = RegressionExperiment()
-    filtered_features = dataset.loc[:, dataset.columns != target_column]
-    filtered_dataset = pd.DataFrame(filtered_features, columns=filtered_features.columns)
-    clf = r.setup(filtered_dataset, target=dataset[target_column],preprocess=False,transformation= False, session_id=2, experiment_name ='Regression',index=False)
-    best_model = clf.compare_models(fold=5,exclude = ['lightgbm','qda','dummy','ridge','catboost','br','et','lasso','huber','llar'])
-    #best_model="Unable Fetch"
-    return gr.Textbox(label="Best Model",value=best_model)
+
     
 # Load the JSON file
 with open('rules_modular_approach.json') as file:
@@ -51,10 +42,7 @@ def global_variable_value_change_show(value):
 with gr.Blocks() as demo:
     gr.Markdown("Hey Team : Gradio -> working fine!")
     file_input = gr.File(label="Upload Dataset",file_types=['.csv'],file_count="single")  
-    best_model = gr.Textbox(label="Best Model",value="NOT SET",visible=True)
-    
-    apply_button = gr.Button("Start Process",variant='primary',visible=True)
-    
+
     
     global_variable_input = gr.Textbox(label="Input Global Variable")
     current_value_list = gr.Textbox(os.environ.get("MY_SECRET_KEY"),visible=False)
@@ -63,7 +51,7 @@ with gr.Blocks() as demo:
     
     submit_button = gr.Button("Submit")
 
-    apply_button.click(pycaret_process,inputs=[file_input],outputs=[best_model])
+
     submit_button.click(global_variable_value_change_show, inputs=[global_variable_input],outputs=[current_value_list,current_value_json,current_value_method])
 
 app = gr.mount_gradio_app(app, demo, path="/")
